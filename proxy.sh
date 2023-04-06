29d4990b290945f71013a6745d59d0330851e759
mashipeng <759094675@qq.com>
2023-04-06 18:14:07 +0800

    auto connect proxy on wsl2 & config python venv on pycharm at wsl2

---
+++ proxy.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# help
+if [ "$1" = "help" ]
+then
+    help   	
+fi
+
+hostip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+wslip=$(hostname -I | awk '{print $1}')
+port=4780
+
+PROXY_HTTP="http://${hostip}:${port}"
+
+help(){
+    echo "set:   set proxy"
+    echo "unset: unset proxy"
+    echo "test:  status of proxy"
+}
+
+set_proxy(){
+    export http_proxy="${PROXY_HTTP}"
+    export HTTP_PROXY="${PROXY_HTTP}"
+
+    export https_proxy="${PROXY_HTTP}"
+    export HTTPS_proxy="${PROXY_HTTP}"
+}
+
+unset_proxy(){
+    unset http_proxy
+    unset HTTP_PROXY
+    unset https_proxy
+    unset HTTPS_PROXY
+}
+
+test_setting(){
+    echo "Host ip:" ${hostip}
+    echo "WSL ip:" ${wslip}
+    echo "Current proxy:" $https_proxy
+}
+
+if [ "$1" = "set" ]
+then
+    set_proxy
+
+elif [ "$1" = "unset" ]
+then
+    unset_proxy
+
+elif [ "$1" = "test" ]
+then
+    test_setting
+else
+    echo "Unsupported arguments."
+fi
